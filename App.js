1a18eccdb600c2a5f78d2cf37249a772d45adbf5
Juan Ignacio Marchetto <43477348+JuanIgnacioMarchetto@users.noreply.github.com>
2023-12-04 10:34:55 -0300

    Actualizar

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GiftList from './GiftList';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Mi Aplicación de Regalos de Navidad</h1>
+      <GiftList />
+    </div>
+  );
+};
+
+export default App;
